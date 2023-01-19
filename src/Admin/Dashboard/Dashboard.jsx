3039e4b00d3186ab1292240b5d879a675724c42f
Jugnusingh <46666515+Jugnusingh@users.noreply.github.com>
2023-01-19 13:43:56 +0530

    view data file removed

--- src/Admin/Dashboard/Dashboard.jsx
+++ src/Admin/Dashboard/Dashboard.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import AdminSidebar from '../../Admin/Dashboard/adminLeftBar/AdminSidebar'
 import './Dashboard.css'
-import ViewData from './viewData/ViewData'
+
 
 const Dashboard = () => {
     return (   
         <div>
             <AdminSidebar/>
-            <ViewData/>
+           
         </div>
     )
 }
