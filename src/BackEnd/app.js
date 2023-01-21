2226542c2a6ad686527299ee5e1623ab79c5c4a5
Jugnusingh <46666515+Jugnusingh@users.noreply.github.com>
2023-01-21 21:52:07 +0530

    add multer library

--- src/BackEnd/app.js
+++ src/BackEnd/app.js
@@ -6,9 +6,6 @@ const bodyParser = require("body-parser")
 
 //middlewares
 app.use(bodyParser.json())
-
-// const studentRoute = require("./api/route/student")
-// const facultyRoute = require("./api/route/faculty")
 const productRoute = require("./api/route/productData")
 const assignmentRoute = require("./api/route/assignment")
 
@@ -16,21 +13,15 @@ mongoose.connect("mongodb://127.0.0.1:27017/DalalTechnologies",{
     useUnifiedTopology: true,
     useNewUrlParser: true
   })
-//   .then((result)=>{
-//     console.log(result,"DB is connected successfully")
-//   }).catch((error)=>{
-//     console.log(error,"Error DB is not connected")
-//   })
 mongoose.connection.on("error",(error)=>{
     console.log("Error DB is not connected") 
 })
 mongoose.connection.on("connected",(connected)=>{
     console.log("DB is connected")
 })
-// app.use("/student",studentRoute)
-// app.use("/faculty",facultyRoute)
 app.use("/product",productRoute)
 app.use("/assignment",assignmentRoute)
+
 app.use("/", (req,res)=>{
     res.status(404).json({
         msg:"Page is not found"
--- src/BackEnd/server.js
+++ src/BackEnd/server.js
@@ -1,6 +1,9 @@
 const http = require("http")
 const app = require("./app")
 const server = http.createServer(app)
+
+
+
 server.listen(4000,()=>{
     console.log("Server is running...")
 })
