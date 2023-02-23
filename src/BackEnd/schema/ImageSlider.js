e9a01cc3a06a754e49add04774700dc4d2c476c0
Jugnusingh <jugnu908@gmail.com>
2023-02-23 14:14:19 +0530

    deleted

--- src/BackEnd/schema/ImageSlider.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const ImageSlider = mongoose.Schema({
-    Title:String,
-    Description:String,
-    ImgUrl:String
-})
-module.exports= mongoose.model("slider",ImageSlider)
